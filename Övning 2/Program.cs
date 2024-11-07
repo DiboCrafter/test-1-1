ea09959a46b2c6854348b99f839463e65d137a87
DiboCrafter <emsv06002@utb.vaxjo.se>
2024-11-07 12:48:42 +0100

    Add project files.

---
+++ Övning 2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Cypass_gay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hej! har du gått ut gymnasiet?");
+            Console.WriteLine("svara j för ja och n för nej");
+            string J 
+                
+             
+             
+        }
+    }
+}
